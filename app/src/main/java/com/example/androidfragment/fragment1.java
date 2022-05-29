1c215f62451a07b2b3b4aec7fd0b3d063fa23577
Charith Lansakara <lansakaracharith@gmail.com>
2022-05-29 23:57:07 +0530

    Android fragment created

---
+++ app/src/main/java/com/example/androidfragment/fragment1.java
@@ -0,0 +1,26 @@
+package com.example.androidfragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link fragment1#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+
+//github test
+public class fragment1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment1, container, false);
+    }
+}
